b3a790f7d281d0e2420355467cd1714d4dd72255
Teddystone08 <ernestlrty@hotmail.com>
2022-09-12 09:26:01 -0700

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - print alphabet
+*
+*Description: using the main function
+*this program prints "programming alphabet followed by new line"
+*Return: 0
+*/
+int main(void)
+{
+int a;
+for (a = 48 ; a <= 57 ; a++)
+{
+	putchar(a);
+if (a != 57)
+{
+	putchar(',');
+	putchar(',');
+}
+}
+putchar('/n')
+return(0);
+}
