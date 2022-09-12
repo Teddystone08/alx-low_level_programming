9f0897e6beebe6fa7dc7a9b9474d47e621e5fb6c
Teddystone08 <ernestlrty@hotmail.com>
2022-09-12 09:09:51 -0700

    base i6

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - print alphabet
+*
+*Description: using the main function
+*this program prints "programming alphabet followed by new line"
+*Return: 0
+*/
+int main(void)
+{
+char c;
+for (c = 'a' ; c <= '9' ; c++)
+{
+	putchar(c);
+}
+for (c = 'a' ; c <= 'f' ; c++)
+{
+	putchar(c);
+}
+putchar('\n');
+return (0);
