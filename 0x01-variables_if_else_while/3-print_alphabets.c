c3fad1adb55cfcd1d25000ba300607ba9f9d1f18
Teddystone08 <ernestlrty@hotmail.com>
2022-09-12 07:18:59 -0700

    alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,8 +2,8 @@
 /**
 *main - print alphabet
 *
-*Description: using the main function 
-*this program prints "programming alphabet followed by new line
+*Description: using the main function
+*this program prints "programming alphabet followed by new line"
 *Return: 0
 */
 int main(void)
