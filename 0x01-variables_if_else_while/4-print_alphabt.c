00275fe29833f39123d290580ed191206ba3c8a4
Teddystone08 <ernestlrty@hotmail.com>
2022-09-12 07:39:01 -0700

    lower alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - print alphabet
+*
+*Description: using the main function
+*this program prints "programming alphabet followed by new line"
+*Return: 0
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+if 
+(ch != 'e' && ch != 'q')
+{
+	putchar(ch);
+}
+}
+putchar('\n');
+return (0);
+}
