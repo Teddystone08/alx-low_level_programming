0a9c6a83bc90d02169c58f06ff70f60b71855572
Teddystone08 <ernestlrty@hotmail.com>
2022-09-12 08:06:20 -0700

    number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - print alphabet
+*
+*Description: using the main function
+*this program prints "programming alphabet followed by new line"
+*Return: 0
+*/
+int main(void)
+{
+int i;
+for (i = 0;i < 10; i++)
+{
+	printf("%i", i);
+}
+printf("\n");
+return (0);
+}
