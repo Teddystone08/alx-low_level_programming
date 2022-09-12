4a3589f9b2dedc522a67d521145533b0a0c5c161
Teddystone08 <ernestlrty@hotmail.com>
2022-09-12 08:26:52 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - print alphabet
+*
+*Description: using the main function
+*this program prints "programming alphabet followed by new line"
+*Return: 0
+*/
+int main(void)
+{
+char c;
+for (c = 'z' ; c >= 'a' ; c--)
+{
+putchar(c);
+}
+putchar("\n");
+return (0);
+}
