1c29f584b15cd352a4d36633968b2ff19ed81366
Teddystone08 <ernestlrty@hotmail.com>
2022-09-12 07:08:43 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - print alphabet
+*
+*Description: using the main function 
+*this program prints "programming alphabet followed by new line
+*Return: 0
+*/
+int main(void)
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	putchar(ch);
+}
+for (ch = 'A' ; ch <= 'Z' ; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
