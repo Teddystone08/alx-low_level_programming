96945a4999a2df3dfcec5fdbb2f75f5d2a6e85e3
Teddystone08 <ernestlrty@hotmail.com>
2022-09-12 17:21:40 -0700

    print_comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,16 +13,15 @@ for (num1 = 0 ; num1 <= 10 ; num1++)
 {
 for (num2 = 0 ; num2 < 10 ; num2++)
 {
-putchar((num1 + '0');
+putchar(num1 + '0');
 putchar(num2 + '0');
 
 if (num1 == 9 && num2 == 9);
-break;
+break ;
 }
 
 putchar(',');
 putchar(' ');
-}
 }
 putchar('\n')
 return (0);
