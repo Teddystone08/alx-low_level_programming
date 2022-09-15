328858fd0b1000d85222d429125e46c1cc5fefcb
Teddystone08 <ernestlrty@hotmail.com>
2022-09-14 20:10:46 -0700

    lower alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * alphabet - print in lowcase
+ *
+ * Return: always 0
+ *
+ */
+int print_alphabet(void)
+{
+	char alpha = 'a';
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\'n)
+		return (0);
+}
