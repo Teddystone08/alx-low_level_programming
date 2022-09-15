6c5adc0b5716fc19da8a29eb6df93c4bb3e834a3
Teddystone08 <ernestlrty@hotmail.com>
2022-09-15 10:01:50 -0700

    main.c,main.h

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void print_alphabet_x10(void);
+int print_alphabet(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void _putchar(int inpt);
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
