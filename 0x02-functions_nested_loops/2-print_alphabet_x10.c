734c36673af828fe87343a80d85998fa57edcf74
Teddystone08 <ernestlrty@hotmail.com>
2022-09-15 06:11:42 -0700

    print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * alphabet - print in lowcase
+ *
+ * Description: @ val from the main.h print in lower cases
+ *
+ * Return: always 0
+ *
+ */
+
+int print_alphabet_x10(void)
+{
+	int j;
+
+	ch = 0;
+	while (ch < 10)
+	{
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(n);
+	}
+	ch++;
+	_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -11,5 +11,5 @@
 
 int _putchar(int val)
 {
-	return (write(1, &c, 1));
+	return (write (1, &c, 1));
 }
