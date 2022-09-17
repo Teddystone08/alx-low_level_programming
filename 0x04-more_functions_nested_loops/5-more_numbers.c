6a35c57d118d8200af321ac89fb07a2a4d6de73f
Teddystone08 <ernestlrty@hotmail.com>
2022-09-17 05:34:40 -0700

    more numb

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -18,9 +18,10 @@ void more_numbers(void)
 			if (n2 > 9)
 			{
 				_putchar((n2 / 10) + '0');
+				_putchar((n2 / 10) + '0');
 			}
-			_putchar((n2 % 10) + '0');
 		}
-		_putchar(10);
-	}
+		_putchar('\n');
 
+	}
+}
