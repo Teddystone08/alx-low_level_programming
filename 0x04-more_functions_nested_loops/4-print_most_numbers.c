c010bded71c408225a89b702459590eb0d3a81bd
Teddystone08 <ernestlrty@hotmail.com>
2022-09-16 20:59:11 -0700

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - entry point
+ *
+ * Description: print the numbers with _putchar
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int count;
+
+	if (count != 2 && count != 4)
+	{
+		putchar(count);
+			count++;
+	}
+	putchar('\n')
+}
