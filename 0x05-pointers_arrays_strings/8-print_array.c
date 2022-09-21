4a2d2dfc64cf8e023f0cb830a82acf83b4724ba9
Teddystone08 <ernestlrty@hotmail.com>
2022-09-21 08:45:12 -0700

    array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,14 +9,14 @@
  *
  * Return: nothing
  */
-void print_array(int *a. int n)
+void print_array(int *a, int n)
 {
 	int b;
 
 	for (b = 0; b < n; b++)
 	{
 	if (b != n - 1)
-		printf("%d, ", a[b]);
+		printf("%d", ',', a[b]);
 	else
 		printf("%d", a[b]);
 	}
