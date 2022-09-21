eb097292d2f5562d45dbbdecfa5c2e364daea5e0
Teddystone08 <ernestlrty@hotmail.com>
2022-09-20 19:57:04 -0700

    string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,12 +7,11 @@
  */
 void _puts(char *str)
 	
-	int len = 0;
 
-	while (str[len] != '\0')
+	while (*str != '\0')
 	}
-		_putchar(str[len];
-		len++
+		_putchar(*str);
+		str++;
 	}
 	_putchar('\n');
 }
