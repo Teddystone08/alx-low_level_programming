ca6d05c98fe140ca74d2c39a71d1912a12207c1d
Teddystone08 <ernestlrty@hotmail.com>
2022-09-20 20:25:06 -0700

    string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,18 @@
 #include "main.h"
-
+# include <stdio.h>
 /**
  * _puts - to print a string to stdout
  * *@str: variable for input
  * Return:no error
  */
 void _puts(char *str)
-	
+}
+	int i = 0;
 
-	while (*str != '\0')
+	while (*(str + i) != '\0')
 	}
-		_putchar(*str);
-		str++;
+		_putchar(*(str +i));
+		i++
 	}
-	_putchar('\n');
+	_putchar(10);
 }
