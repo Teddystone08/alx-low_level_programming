d68c58eae1b3cbcc246b34d39ef0dce966579c17
Teddystone08 <ernestlrty@hotmail.com>
2022-10-01 11:36:41 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		puts_recursion(s + 1)
+		_putchar('\n');
 	}
 	else
-		_putchar('\n');
-	
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
 }
