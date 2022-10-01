8540e6c5eb9d20692008acf87e6cac1337871243
Teddystone08 <ernestlrty@hotmail.com>
2022-10-01 13:13:13 -0700

    lenth recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - sum the length of a string
+ * @s: string value
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int sum = 0;
+
+	if (*s != '\0')
+	{
+		sum++;
+		sum += _strlen_recursion(s + 1);
+	}
+	return (sum);
+}
