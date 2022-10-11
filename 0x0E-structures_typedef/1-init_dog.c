c7faaf849fedee110585bf48049b84bcf8e6548b
Teddystone08 <ernestlrty@hotmail.com>
2022-10-11 02:12:53 -0700

    print dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+# include "dog.h"
+
+/**
+ * init_dog - function that initialize a type struct dog
+ * @d: passing structure at the function
+ * @name: store a string fot main
+ * @age: store anumber for main
+ * @owner: store a string for main
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
