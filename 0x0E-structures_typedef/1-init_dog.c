952e4ca00b61f4eab603a8e45af2a3bec2ea80b8
Teddystone08 <ernestlrty@hotmail.com>
2022-10-11 04:44:25 -0700

    print dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,10 +10,13 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 {
-	if (d != NULL)
+	if (d == NULL)
+		return;
+	else
 	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
 	}
+	
 }
