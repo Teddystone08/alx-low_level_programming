3724b7403931d896f4b8df9c9cb89b6551440906
Teddystone08 <ernestlrty@hotmail.com>
2022-10-23 14:08:48 -0700

    free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,16 +5,13 @@
  * @head: head of linked list
  * Return: void
  */
-
 void free_list(list_t *head)
 {
-	list_t *tmp;
-
-	while (head)
+	list_t *temp;
+	
+	for (temp = head; temp != NULL; temp = temp->next)
 	{
-		tmp = tmp->next;
-		free(head->str);
-		free(head);
-		head = tmp;
+		free(temp->str);
+		free(temp);
 	}
 }
