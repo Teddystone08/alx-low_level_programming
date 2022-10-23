69176939944ba3d6fa56033118561b2dd34f53aa
Teddystone08 <ernestlrty@hotmail.com>
2022-10-23 11:55:29 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,18 +9,14 @@ size_t print_list(const list_t *h)
 {
 	unsigned int i = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
-		{
-			printf("[0] (nil)");
-		}
+			printf("[0] (nil)\n");
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-		}
-		h = h->next;
 		i++;
+		h = h->next;
 	}
 
 	return (i)
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef lists_H
-#define lists_H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,6 +17,7 @@
 typedef struct list_s
 {
 	char *str;
+
 	usigned int len;
 	struct list_s *next;
 } list_t;
@@ -27,4 +28,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
+#endif/*LISTS_H*/
