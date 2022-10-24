e00bec84a892fa857073a8387a7863eced780891
Teddystone08 <ernestlrty@hotmail.com>
2022-10-24 04:04:38 -0700

    hello holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,11 @@
 SECTION .data
-msg:	db "hello, Holberton", 0
+msg:	db "Hello, Holberton", 0
 fmt:	db "%s", 10, 0
 
 	SECTION .text
 	extern printf
 	global main
-main:;
+main:
 	mov esi, msg
 	mov edi, fmt
 	mov eax, 0
