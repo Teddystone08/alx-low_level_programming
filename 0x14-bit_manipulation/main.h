0f0343b521e712aea3e965b13c3ef58f16de48fb
Teddystone08 <ernestlrty@hotmail.com>
2022-10-28 08:32:25 -0700

    binary to unit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,23 +1,10 @@
 #ifndef main_h
 #define main_h
 
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-/**
- * _putchar - writes the character c to stdout
- *
- * @c: The character to print
- *
- * Return: On success 1.
- *
- * On error, -1 is returned, and errno is set appropriately.
- *
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
 
 unsigned int binary_to_uint(const char *b);
 
