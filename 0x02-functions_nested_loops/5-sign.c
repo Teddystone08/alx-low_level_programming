1b8f700c48ae9c21a5b8b20fb298d3b33f006e51
Teddystone08 <ernestlrty@hotmail.com>
2022-11-23 20:33:16 +0000

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - function nested loops
+ * print_signi - function nested loops
  *
  * Description: print out sign of numbers
  *
@@ -9,20 +9,20 @@
 
 int print_sign(int n)
 {
-	int test;
+
 	if (n > 0)
 	{
-		test = 1;
 		_putchar('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
-		test = 0;
 		_putchar('0');
+		return (0);
 	}
 	else
 	{
-		test = -1;
 		_putchar('-');
+		return (-1);
 	}
-	return (0);
+}
