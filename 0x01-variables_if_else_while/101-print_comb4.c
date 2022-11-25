f2b4b81670e57eeb8cc8b39f3967624df65efd9a
Teddystone08 <ernestlrty@hotmail.com>
2022-11-25 21:02:54 +0000

    comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,27 +7,36 @@
  */
 int main(void)
 {
-	int d1, d2, d3
+	int i = '0';
+	int j = '0';
+	int k = '0';
 
-	for (d1 = 0; d1 < 8; d1++)
+	while (i <= '7')
 	{
-		for (d2 = d1 + 1; d2 < 9; d2++)
+		while (j <= '8')
 		{
-			for (d3 = d2 + 1; d3 < 10; d3++)
+			while (k <= '9')
 			{
-				putchar((d1 % 10) + '0');
-				putchar((d2 % 10) + '0');
-				putchar((d3 % 10) + '0');
+				if (i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (!(i == '7' && j == '8' && k == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
 
-				if (d1 == 7 && d2 == 8 && d3 == 9)
-					continue;
-
-				putchar(',');
-				putchar(' ');
+				}
+				k++;
 			}
+			k = '0';
+			j++;
 		}
+		j = '0';
+		i++;
 	}
-
 	putchar('\n');
-
 	return (0);
+}
