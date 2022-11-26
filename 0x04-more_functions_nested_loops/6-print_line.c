d1da91a009032cdc1d1ed10e26a14b640ca645ba
Teddystone08 <ernestlrty@hotmail.com>
2022-11-26 16:50:44 +0000

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,12 +8,12 @@
  * Return: nothing
  *
  */
-
 void print_line(int n)
 {
-	while (n-- > 0)
-	{
-		putchar('_');
-	}
-	putchar('\n');
+	int i;
+
+	if (n > 0)
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	_putchar('\n');
 }
