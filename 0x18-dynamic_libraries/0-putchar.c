94e5e7a4215a3d7a18b2c2b0f6ecf95124d817b0
Teddystone08 <ernestlrty@hotmail.com>
2022-12-13 10:37:15 +0000

    libdynamic.so

---
+++ 0x18-dynamic_libraries/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* main - print putchar
+*
+*Description: out oof the main print putchar
+*
+*Return: void
+*/
+int main(void)
+{
+char func[8] = "_putchar";
+int count  = 0;
+int arrlen = sizeof(func) / sizeof(*func);
+while (count < arrlen)
+{
+_putchar(func[count]);
+count++;
+}
+_putchar('\n');
+return (0);
+}
---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strncat - concatenate two string but add inputted number
+ * @dest: sting to be appended
+ * @src: strong to be completed at end of dest
+ * @n:integer parameter to compare index
+ * Return: return the pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int count = 0, count2 = 0;
+
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+
+	while (count2 < n)
+
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;										}
+	return (dest);
+}
