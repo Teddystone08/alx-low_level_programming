0c4e979607236ad0eceaa7335e9427bfdb0ace3a
Teddystone08 <ernestlrty@hotmail.com>
2024-02-28 19:08:50 +0000

    index value

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_bit - function that retuen the value of a bit
+ * @index: index from 0
+ * @n: value
+ * Return: -1 when error else value of bit
+ *
+ */
+
+int get_bit(unsigned long int n, unsigned int index);
+{
+	if (index < sizeof(unsigned long int) * 8)
+		return (-1);
+
+	n = n >> index;
+	return (n & 1);
+}
