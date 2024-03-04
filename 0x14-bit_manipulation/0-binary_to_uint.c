260ef0f7d8b457475ba7671ed1da850e7ad0953e
Teddystone08 <ernestlrty@hotmail.com>
2024-03-04 13:34:24 +0000

    convert

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,28 +3,30 @@
 /**
  * binary_to_uint - function to convert binary to unsigned integer
  * @b: binary number pointer
- * Return: 0 if b has 1 or more char
+ * Return: 0 othewise number
  *
  */
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
+	unsigned int fig;
+	int a, i = 0;
 
-	if (b != NULL || *b == '\0')
-	{
+	if (b == NULL)
 		return (0);
-	}
-	while (*b != '\0')
+
+	while (b[i] != '\0')
 	{
-		if (*b != '0' && *b != '1')
-		{
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		}
-	result = (result << 1) | (*b - '0');
-
-	b++;
+		i++;
 	}
+	fig = 0;
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		fig = fig << 1;
 
-	return (result);
+		if (b[a] == '1')
+			fig = fig + 1;
+	}
+	return (fig);
 }
