87d24ec341bc2d008d7e8c1141d1c9d276720bfc
Teddystone08 <ernestlrty@hotmail.com>
2024-03-04 11:37:14 +0000

    zero

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,11 +9,8 @@
 
 int clear_bit(unsigned long int *n, unsigned int index);
 {
-	if (index >= (sizeof(*n) * 8))
-	{
+	if (index > 8)
 		return (-1);
-	}
 	*n = *n & ~(1 << index);
-
 	return (1);
 }
