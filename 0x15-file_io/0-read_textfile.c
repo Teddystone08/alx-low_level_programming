0104672a7c41d9ad9bcc90bc29540f2553eee23b
Ernest T. Lartey <62164400+Teddystone08@users.noreply.github.com>
2024-03-07 23:19:52 +0000

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * read_textfile - function that reads a text file and prints
  * @filename: variable pointer
@@ -13,24 +12,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t file, let, w;
 	char *text;
-
+	
 	text = malloc(letters);
 	if (text == NULL)
 		return (0);
-
 	if (filename == NULL)
 		return (0);
-
 	file = open(filename, O_RDONLY | O_CREAT);
-
 	if (file == -1)
 	{
 		free(text);
 		return (0);
 	}
-
 	let = read(file, text, letters);
-
 	w = write(STDOUT_FILENO, text, let);
 
 	close(file);
