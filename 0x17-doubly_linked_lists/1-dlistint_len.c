fb6702a99c38d25f07ef63623ec130ca9036bd1e
Teddystone08 <ernestlrty@hotmail.com>
2024-03-24 18:15:54 +0000

    all nodes

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,6 +1,21 @@
-#include "list.h"
+#include "lists.h"
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ * dlistint_len - function that returns number of element
+ * @h: pointer to the head list
+ * Return:number of nodes in list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t head = 0;
+	const dlistint_t *c = h;
 
-
+	while (c != NULL)
+	{
+		head++;
+		c = c->next;
+	}
+	return (head);
+}
