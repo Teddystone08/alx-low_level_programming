cd62879013b70cf83a390b15efbed423c5664d6a
Teddystone08 <ernestlrty@hotmail.com>
2024-03-24 17:09:01 +0000

    node

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,10 +11,9 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t rtun = 0;
 	while (h != NULL)
 	{
-		printf("%d\n", h->next);
+		printf("%d\n", h->n);
+		h = h->next;
 		rtun++;
 	}
-	return (rtun)
-
-
+	return (rtun);
 }
