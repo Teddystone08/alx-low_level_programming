02d337a482c7f06804473f5ff413db740f24ec2b
Teddystone08 <ernestlrty@hotmail.com>
2024-03-24 18:19:44 +0000

    all nodes

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -10,12 +10,11 @@
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t head = 0;
-	const dlistint_t *c = h;
+	const dlistint_t *c;
 
-	while (c != NULL)
+	for (c = h; c != NULL; c = c -> next)
 	{
 		head++;
-		c = c->next;
 	}
 	return (head);
 }
