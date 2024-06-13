7a6503cab0d57e59c7fa306dfbcf19933f657baf
Teddystone08 <ernestlrty@hotmail.com>
2024-06-13 15:58:32 +0000

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - new struct of dog with name, age and owner
+ * @name: identity of dog
+ * @age: years of the dog
+ * @owner: owner of the dog
+ **/
 
 struct dog
 {
 	char *name;
-	float *age;
+	float age;
 	char *owner;
 };
 
