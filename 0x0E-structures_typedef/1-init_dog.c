a4be495d4247110aff9005b0093fe28c3589664f
Teddystone08 <ernestlrty@hotmail.com>
2024-06-13 17:20:33 +0000

    dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,14 +1,13 @@
 # include "dog.h"
-# include <stdlib.h>
+
 /**
  * init_dog - function that initialize a type struct dog
  * @d: passing structure at the function
  * @name: store a string fot main
  * @age: store anumber for main
  * @owner: store a string for main
- */
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
+ **/
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 		return;
